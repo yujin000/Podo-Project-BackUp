ca0ef896f6ff343d74138f1dc3db7642c120e14a
Jung-taekku-dev <xorrn93@gmail.com>
2022-11-04 09:21:38 +0900

    Feat :  패스워드 값에 SHA512 보안 기능 추가

--- src/main/java/DAO/MemberDAO.java
+++ src/main/java/DAO/MemberDAO.java
@@ -1,5 +1,7 @@
 package DAO;
 
+import java.math.BigInteger;
+import java.security.MessageDigest;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -27,17 +29,30 @@ public class MemberDAO {
 		return ds.getConnection();
 	}
 	
+	public static String getSHA512(String input) {
+        String toReturn = null;
+        try {
+            MessageDigest digest = MessageDigest.getInstance("SHA-512");
+            digest.reset();
+            digest.update(input.getBytes("utf8"));
+            toReturn = String.format("%0128x", new BigInteger(1, digest.digest()));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return toReturn;
+    }
+	
 	public int signup(String email,	String pw, 
 	  String nickname,String name,
 	 int phone
 			) throws Exception {
-		String sql = "insert into member values(?,?,'basic',null,sysdate,null,?,?,?)";
+		String sql = "insert into member values(?,?,'basic',null,sysdate,null,?,?,?,null,1)";
 
 		try(	Connection con = this.getConnection();
 					PreparedStatement pstat = con.prepareStatement(sql);) {
 					
 				pstat.setString(1, email);
-				pstat.setString(2, pw);
+				pstat.setString(2, getSHA512(pw));
 				pstat.setString(3, nickname);
 				pstat.setString(4,name);
 				pstat.setInt(5, phone);
@@ -66,7 +81,7 @@ public class MemberDAO {
 				PreparedStatement pstat = con.prepareStatement(sql);
 				){
 			pstat.setString(1,email);
-			pstat.setString(2,pw);
+			pstat.setString(2,getSHA512(pw));
 			
 			System.out.println(email+pw);
 			ResultSet rs = pstat.executeQuery();
