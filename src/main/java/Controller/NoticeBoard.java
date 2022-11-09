c062298aba1d022e3f6fe7527793fd6fbb8e6ad8
박세진 <crazywarden@naver.com>
2022-11-09 16:50:52 +0900

    (ver 4.7.0) (Feat : NoticeBoard.java, NoticeBoardDAO.java, adminNotice.jsp noticeWrite.jsp) 공지사항 작성 기능 추가 (출력 아직 미구현) (/src/summerNote) 서머노트API추가) (Remove : (Admin.jsp) 더미파일 제거.)

--- src/main/java/Controller/NoticeBoard.java
+++ src/main/java/Controller/NoticeBoard.java
@@ -1,17 +1,39 @@
 package Controller;
 
 import java.io.IOException;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import DAO.NoticeBoardDAO;
+import DTO.NoticeBoardDTO;
+
 @WebServlet("*.notice")
 public class NoticeBoard extends HttpServlet {
 	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("utf8");
+		// post의 한글깨짐현상을 처리하기 위한 로직
 		
+		String uri = request.getRequestURI();
+		try {
+			if (uri.equals("/write.notice")) {
+				NoticeBoardDAO dao = NoticeBoardDAO.getInstance();
+				String noticeWriter = request.getSession().getAttribute("loginNickname").toString();
+				String noticeTitle = request.getParameter("noticeTitle");
+				String noticeContents = request.getParameter("noticeContents");
+				String noticeCategory = request.getParameter("noticeCategory");
+								
+				NoticeBoardDTO dto = new NoticeBoardDTO(0, noticeWriter, noticeTitle, noticeContents, null, noticeCategory);
+				int result = dao.writeNoticeBoard(dto);
+				response.sendRedirect("/admin/adminNotice/adminNotice.jsp");
+			}
+		} catch (Exception e) {
+			
+		}
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
