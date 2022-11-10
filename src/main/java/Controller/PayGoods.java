a9f5d10e9f654e21946c4caf20fdb492ac19f5fb
kyc <yechan3110@gmail.com>
2022-11-10 16:08:07 +0900

    Feat: 관리자페이지 멤버십 CRUD기능 구현

--- src/main/java/Controller/PayGoods.java
+++ src/main/java/Controller/PayGoods.java
@@ -26,6 +26,31 @@ public class PayGoods extends HttpServlet {
 				List<PayGoodsDTO> goodsList = dao.selectAll();
 				request.setAttribute("goodsList", goodsList);
 				request.getRequestDispatcher("/admin/adminMembership.jsp").forward(request, response);
+			}else if(uri.equals("/update.goods")) {
+				String payGoodsName = request.getParameter("payGoodsName");
+				int payGoodsPrice = Integer.parseInt(request.getParameter("payGoodsPrice"));
+				String payGoodsExp = request.getParameter("payGoodsExp");
+				String payGoodsType = request.getParameter("payGoodsType");
+				String payGoodsInfo = request.getParameter("payGoodsInfo");
+				int payGoodsSeq = Integer.parseInt(request.getParameter("payGoodsSeq"));
+				PayGoodsDAO dao = PayGoodsDAO.getInstance();
+				dao.update(payGoodsName, payGoodsPrice, payGoodsExp, payGoodsType, payGoodsInfo, payGoodsSeq);
+				response.sendRedirect("/goodsList.goods");
+			}else if(uri.equals("/delete.goods")) {
+				int seq = Integer.parseInt(request.getParameter("seq"));
+				PayGoodsDAO dao = PayGoodsDAO.getInstance();
+				dao.delete(seq);
+				response.sendRedirect("/goodsList.goods");
+			}else if(uri.equals("/addProducts.goods")) {
+				String payGoodsName = request.getParameter("payGoodsName");
+				int payGoodsPrice = Integer.parseInt(request.getParameter("payGoodsPrice"));
+				String payGoodsExp = request.getParameter("payGoodsExp");
+				String payGoodsType = request.getParameter("payGoodsType");
+				String payGoodsInfo = request.getParameter("payGoodsInfo");
+				PayGoodsDAO dao = PayGoodsDAO.getInstance();
+				PayGoodsDTO dto = new PayGoodsDTO(0,payGoodsName,payGoodsPrice,payGoodsExp,payGoodsType,payGoodsInfo);
+				dao.addProducts(dto);
+				response.sendRedirect("/goodsList.goods");
 			}
 		}catch(Exception e) {
 			e.printStackTrace();
